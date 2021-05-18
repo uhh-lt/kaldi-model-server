1113d7bb149167493f1889fb2aa0654dc7a5e65d
Steffen <remstef@users.noreply.github.com>
2021-05-18 17:36:41 +0200

    Update download_example_models.sh

--- download_example_models.sh
+++ download_example_models.sh
@@ -1,5 +1,11 @@
 mkdir models/
 cd models/
 mv ../kaldi_tuda_de_nnet3_chain2.yaml ./
+
+# German
 wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
 tar xvfj de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
+
+# English
+wget http://ltdata1.informatik.uni-hamburg.de/pykaldi/en_160k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
+tar xvfj en_160k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
